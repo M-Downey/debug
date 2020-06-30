c44e5997cc01580c49581a992e6aa8adcdde3337
tony <923940358@qq.com>
2020-06-30 16:03:10 +0800

    finish 10

---
+++ 10/4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+	int num;
+    scanf("%d", &num);
+    int digit;
+    int a;
+    for(digit = 2; digit <= num; digit++) {
+        for (a = 2; a < digit; a++) {
+            if (digit % a == 0) {
+                break;
+            }
+        }
+        if (a == digit) {
+        	printf("%d\n", a);
+    	}
+    }
+
+    return 0;
+}

cdeab09e22934bb4d14410fbb0f1d7ce1d2a00e5
guanliliang123 <55387672+guanliliang123@users.noreply.github.com>
2020-06-30 15:58:07 +0800

    Update 9.c

--- 5/9.c
+++ 5/9.c
@@ -10,20 +10,20 @@ int main()
     
     for(i = 0; i < 9; i++){
     	for(j = 0;j < 9 - i; j++){
-    		if(strcmp(name[j], name[j+1]) > 0){
+    	    if(strcmp(name[j], name[j+1]) > 0){
                 char temp[21];
                 strcpy(temp, name[j]);
                 strcpy(name[j], name[j+1]);
                 strcpy(name[j+1], temp);
             }
-		} 
+	} 
     }
 
     for(i = 0; i < 10; i++){
         printf("%s", name[i]);
         if(i != 9){
-        	printf("\n");
-		}
+            printf("\n");
+	}
     }
     
     return 0;

5afd15494e4ceb8e1e102967c28506b777ec5ae9
M-Downey <60745835+M-Downey@users.noreply.github.com>
2020-06-30 16:40:34 +0800

    Update 3.c

--- 9/3.c
+++ 9/3.c
@@ -2,19 +2,19 @@
 
 long long int fun(int n)
 {	
-	if (n == 1) {
-		return 0;
-	} else if (n == 2){
+    if (n == 1) {
+    	return 0;
+    } else if (n == 2){
     	return 1;
-	} else if (n == 3) {
-		return 1;
-	} else {
-    	return fun(n-2)+fun(n-3);
-	}
+    } else if (n == 3) {
+	return 1;
+    } else {
+    	return fun(n - 2) + fun(n - 3);
+    }
 }
 int main() 
 {
-	long long int n;
+    long long int n;
     scanf("%lld", &n);
     printf("%lld", fun(n));
     return 0;

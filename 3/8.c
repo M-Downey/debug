bedebe0b019174b1f10b6dd20d5608df4687ca4a
guanliliang123 <55387672+guanliliang123@users.noreply.github.com>
2020-06-30 16:16:25 +0800

    Update 8.c

--- 3/8.c
+++ 3/8.c
@@ -11,26 +11,26 @@ int main()
 	scanf("%d %d", &m, &n);
 
 	for(i = 0; i < m; i++){
-		for(j = 0; j < n; j++){
-			scanf("%d", &matrix_1[i][j]);
-		}
+	    for(j = 0; j < n; j++){
+		scanf("%d", &matrix_1[i][j]);
+	    }
 	}
 	
 		
 	while(m > 0 && n > 0){	
 	    for(j = 0 ; j < n ; j++){
-			printf("%d", matrix_1[0][j]);
-			if(m == 1 && j == n - 1){
-				return 0;
-			}
-			else{
-				printf(" ");
-			}    
+		printf("%d", matrix_1[0][j]);
+		if(m == 1 && j == n - 1){
+		    return 0;
 		}
+		else{
+		    printf(" ");
+		}    
+	    }
 	    
 		for(i = 0; i < n ; i++){
-			for(j = 0 ; j < m - 1 ; j++){
-				matrix_2[i][j] = matrix_1[j + 1][n - 1 - i];
+		    for(j = 0 ; j < m - 1 ; j++){
+			matrix_2[i][j] = matrix_1[j + 1][n - 1 - i];
 			}
 		}
 		 

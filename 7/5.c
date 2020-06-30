1642f9682e9fea85532a8a2ec6b75a5e681bb583
M-Downey <60745835+M-Downey@users.noreply.github.com>
2020-06-30 16:51:13 +0800

    Update 5.c

--- 7/5.c
+++ 7/5.c
@@ -7,31 +7,31 @@ int main()
     if (month >= 1 && month <= 12) { 
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
      		if (day >= 1 && day <= 31) {
-             	printf("YES");  
-			} else {
-				printf("NO");
+             	    printf("YES");  
+		} else {
+		    printf("NO");
             }
      	} else if(month == 4 || month == 6 || month == 9 || month == 11) {
-               if(day >=1 && day <= 30){
-                	printf("YES");
-				} else {
-					printf("NO");
+               if (day >=1 && day <= 30) {
+                    printf("YES");
+		} else {
+		    printf("NO");
                 }      
     	 } else if((year % 100 != 0 && year % 4 == 0) || year % 400 ==0) {
-               if(day >= 1 && day <= 29){
-                	printf("YES");
-				} else {
-					printf("NO");
-				}
-     	} else if (year % 4 != 0 || (year % 100 == 0 && year %400 != 0)) {
-                if(day >= 1 && day <= 28){
+               if (day >= 1 && day <= 29) {
                     printf("YES");
-				} else {
-					printf("NO");
+		} else {
+		    printf("NO");
+		}
+     	} else if (year % 4 != 0 || (year % 100 == 0 && year %400 != 0)) {
+                if (day >= 1 && day <= 28) {
+                    printf("YES");
+		} else {
+		    printf("NO");
                 }    
         }   
 	} else {
-		printf("NO");
+	        printf("NO");
     } 
     return 0;
 }

aee085c26021c30f3ba6bf7b537f21302878c407
guan <1402234866@qq.com>
2020-06-30 15:12:01 +0800

    finish4

---
+++ 4/9.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) 
+{
+    int c;
+    if(*a < *b){
+        c = *a;
+        *a = *b;
+        *b = c;
+    }
+}
+
+int main() 
+{
+    int n = 10;
+    int numbers[10];
+    int i;
+    int j; 
+    
+    // 读入给定的数字
+    for (i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    
+    for (i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            	swap(&numbers[i], &numbers[j]);
+        }
+    }
+    
+    for (i = 0; i < n; i++) {
+        printf("%d", numbers[i]);
+        if(i != n - 1) {
+        	printf(" ");
+		}
+    }
+    
+    return 0;
+}

dbc11b83e2f15474b01b22b3b6a736f8679ac78e
M-Downey <60745835+M-Downey@users.noreply.github.com>
2020-06-30 16:34:29 +0800

    Update 1.c

--- 9/1.c
+++ 9/1.c
@@ -7,16 +7,16 @@ int cal_step(int a[], int n)
 
 int main() 
 {
-	int N;
+    int N;
     scanf("%d", &N);
     int step[55];
     step[0] = 0;
     step[1] = 0;
     step[2] = 1;
     step[3] = 1;
-	for (int i = 4; i <= N; i++) {
-		step[i] = step[i - 2] + step[i - 3];
-	}
-	printf("%d", step[N]); 
+    for (int i = 4; i <= N; i++) {
+	step[i] = step[i - 2] + step[i - 3];
+    }
+    printf("%d", step[N]); 
     return 0;
 }
